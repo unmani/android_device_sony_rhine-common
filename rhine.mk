2a898cc22b5d6d6dc48278a52ffe82e28c089aa9
David <Davidteri91@gmail.com>
2016-10-17 11:15:33 +0200

    rhine: remove not needed BT script
    on property:bluetooth.hciattach=true
        setprop bluetooth.status on
    
    on property:bluetooth.hciattach=false
        setprop bluetooth.status off
    
    Change-Id: I6ff013f8403f4da70569252c2b4f626bb42cbe71
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- rhine.mk
+++ rhine.mk
@@ -44,7 +44,6 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/rootdir/fstab.qcom:root/fstab.qcom \
     $(COMMON_PATH)/rootdir/init.qcom.rc:root/init.qcom.rc \
     $(COMMON_PATH)/rootdir/init.shims.rc:root/init.shims.rc \
-    $(COMMON_PATH)/rootdir/system/etc/init.qcom.bt.sh:system/etc/init.qcom.bt.sh \
     $(COMMON_PATH)/rootdir/init.sony.usb.rc:root/init.sony.usb.rc \
     $(COMMON_PATH)/rootdir/ueventd.qcom.rc:root/ueventd.qcom.rc
 
