3e6131c95e0ee8b77041a34f31a247d5336a689d
Ondrej Hlavac <hlavaco@gmail.com>
2017-10-17 17:48:26 +0200

    rhine-common: Update sbin
    From stock.
    wait4tad again in use probably...

--- rhine.mk
+++ rhine.mk
@@ -51,7 +51,8 @@ PRODUCT_COPY_FILES += \
 
 # Sbin
 PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/rootdir/sbin/tad_static:system/bin/tad_static
+    $(COMMON_PATH)/rootdir/sbin/tad_static:system/bin/tad_static \
+    $(COMMON_PATH)/rootdir/sbin/wait4tad_static:system/bin/wait4tad_static
 
 # ANT+
 PRODUCT_PACKAGES += \
